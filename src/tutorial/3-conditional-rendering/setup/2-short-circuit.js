1e3eeb58f0b7438dc9ec44587c66c647948b00c9
Dylan Cramer <dcrame2@gmail.com>
2022-12-20 11:11:41 -0600

    turnary and short circuit cont

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,15 +4,21 @@ import React, { useState } from "react";
 
 const ShortCircuit = () => {
   const [text, setText] = useState("");
-  const firstValue = text || "hello world";
-  const secondValue = text && "hello world";
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
 
   return (
     <>
       {/* <h1>{firstValue}</h1>
       <h1>value : {secondValue}</h1> */}
       <h1>{text || "john doe"}</h1>
-      {text && <h1>Hello World</h1>}
+      {/* Creating a toggle state  */}
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? <p>There is an error...</p> : <div>There is no error...</div>}
     </>
   );
 };
